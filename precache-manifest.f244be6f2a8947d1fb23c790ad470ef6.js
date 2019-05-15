1148bf26bbce6b7f6de673b89133be7124843407
Jason Kao <jason.kao@columbia.edu>
2019-05-15 01:32:55 -0400

    Updates

---
+++ precache-manifest.f244be6f2a8947d1fb23c790ad470ef6.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "3adeb5b7f3ddd94a5a1d",
+    "url": "/awn-photo-essay/static/js/main.e4e8250d.chunk.js"
+  },
+  {
+    "revision": "8456b348b631a6096db7",
+    "url": "/awn-photo-essay/static/js/runtime~main.3175b651.js"
+  },
+  {
+    "revision": "b7646c45783cb4217d6b",
+    "url": "/awn-photo-essay/static/js/2.6266b1a3.chunk.js"
+  },
+  {
+    "revision": "530da4768d4f67b86bd0304819a9ccab",
+    "url": "/awn-photo-essay/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/awn-photo-essay/precache-manifest.f244be6f2a8947d1fb23c790ad470ef6.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/awn-photo-essay/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
